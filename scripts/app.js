89430428e1bba928054ea092962a14d1742a02b6
LucasMcL <LMcLaughlin09@gmail.com>
2017-01-17 11:37:53 -0600

    init angular app and route provider

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,24 @@
-console.log('hello world')
+////////////////////////////
+// Angular App
+////////////////////////////
+
+var app = angular.module('UserNotes', ['ngRoute'])
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/login', {
+			controller: 'LoginCtrl',
+			templateUrl: 'partials/login.html'
+		})
+		.when('/registration', {
+			controller: 'RegistrationCtrl',
+			templateUrl: 'partials/registration.html'
+		})
+		.when('/new-note', {
+			controller: 'NewNoteCtrl',
+			templateUrl: 'partials/new-note.html'
+		})
+		.when('/notes', {
+			controller: 'NotesCtrl',
+			templateUrl: 'partials/notes.html'
+		})
+})
